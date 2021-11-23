1ccdc2f4eb91686665a9bae2b2ac2efa9761affc
klmlboom <klmlboom1013@gmail.com>
2021-11-23 23:38:33 +0900

    first commit

---
+++ src/main/java/com/mypro/springRestFulDemo/SpringRestFulDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mypro.springRestFulDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestFulDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestFulDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypro/springRestFulDemo/SpringRestFulDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mypro.springRestFulDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+class SpringRestFulDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		log.info("start");
+	}
+
+}
